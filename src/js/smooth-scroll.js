7146a50b8de5a47cd009335e3380a2cf1582341f
Taras <kachmar.taras@gmail.com>
2024-10-09 20:18:05 +0300

    update smooth-scroll

--- src/js/smooth-scroll.js
+++ src/js/smooth-scroll.js
@@ -18,24 +18,7 @@ window.addEventListener('load', function () {
   }
 });
 
-document.querySelectorAll('a.site-nav__link').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    const href = this.getAttribute('href').substring(1);
-    const scrollTarget = document.getElementById(href);
-
-    let topOffset = 0;
-    topOffset = topOffsetSelector();
-
-    const elementPosition = scrollTarget.getBoundingClientRect().top;
-    const offsetPosition = elementPosition - topOffset;
-
-    window.scrollBy({ top: offsetPosition, behavior: 'smooth' });
-  });
-});
-
-document.querySelectorAll('a.footer__link').forEach(link => {
+document.querySelectorAll('[data-link]').forEach(link => {
   link.addEventListener('click', function (e) {
     e.preventDefault();
 
